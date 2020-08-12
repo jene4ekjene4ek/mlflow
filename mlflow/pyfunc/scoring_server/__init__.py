9f13e87d9b6101f612281a662cac760b97198b0b
Yevheniia Bohomaz <yevheniia.bohomaz1@ibm.com>
2020-08-12 11:13:29 +0300

    3

--- mlflow/pyfunc/scoring_server/__init__.py
+++ mlflow/pyfunc/scoring_server/__init__.py
@@ -72,9 +72,11 @@ def parse_json_input(json_input, orient="split", schema: Schema = None):
     # pylint: disable=broad-except
     try:
         print(json_input, 'JSON_INPUT')
-        d = json_input.get('data')[0][0]
-        d = d*2
-        json_input.update({"data": [[d]]})
+        d = json.loads(json_input)
+        a = d.get('data')[0][0]
+        a = a*2
+        d.updates({'data': [[a]]})
+        json_input = json.dumps(d)
 
         return _dataframe_from_json(json_input, pandas_orient=orient, schema=schema)
     except Exception:
